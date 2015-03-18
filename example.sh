84dafc520b944ebcd11ad1cbe878c8f48e736337
RobClap <robclap8@gmail.com>
2015-03-18 23:42:18 +0100

    Created an intro and an example, given the right names to the right files.

---
+++ example.sh
@@ -0,0 +1,1 @@
+cat testfile | ./spydreader --wpm 300
--- prova.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for x in {1..6}
-do
-		printf '  \033[00mbi\033[01;31ma\033[00mnco\r'
-		usleep 250000
-		printf '   \033[00mr\033[01;31mo\033[00msso\r'
-		usleep 250000
-done
-
